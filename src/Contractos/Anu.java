6b13f3a624eaa67fcd0379a942a54c4d2585d65d
bhaskarbala <bhaskar3718@gmail.com>
2019-06-01 13:43:42 +0530

    new class add

---
+++ src/Contractos/Anu.java
@@ -0,0 +1,12 @@
+package Contractos;
+
+public class Anu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Boss";
+		System.out.println(s.length());
+
+	}
+
+}
